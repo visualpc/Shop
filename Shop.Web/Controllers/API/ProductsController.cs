fdaaa4596c15bd64bb1ee7c0c5b959971263c150
Pablo Cuello <pablocuelloc@hotmail.com>
2020-06-24 19:51:16 -0500

    API Products Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[Controller]")]
+
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
